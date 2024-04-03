51004b56f3fb30fdeb6c1b3bbc0633b49a925586
Arthur Alves de Melo <86849969+arthuralvesQA@users.noreply.github.com>
2024-04-03 11:06:00 -0300

    Algoritmo Implementado

--- SequenciaFibonacci/Program.cs
+++ SequenciaFibonacci/Program.cs
@@ -4,9 +4,24 @@ namespace SequenciaFibonacci
 {
     class Program
     {
+        static void Fibonacci(int valor, int valor1 = 1, int valor2 = 1)
+        {
+            if (valor > 0)
+            {
+                Console.WriteLine(valor1);
+                valor1 += valor2;
+                Fibonacci(valor - 1, valor2, valor1);
+            }
+        }
         static void Main()
         {
+            int valores;
 
+            Console.WriteLine("Quantidade de valores a serem exibidos: ");
+            valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sequência Fibonacci: ");
+            Fibonacci(valores);
         }
     }
 }
