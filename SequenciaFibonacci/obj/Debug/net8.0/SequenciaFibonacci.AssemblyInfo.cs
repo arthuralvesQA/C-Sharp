596118ea50e1b9e076a4546c61429dfe17dfcef3
Arthur Alves de Melo <86849969+arthuralvesQA@users.noreply.github.com>
2024-04-05 20:17:45 -0300

    Updated code

--- SequenciaFibonacci/obj/Debug/net8.0/SequenciaFibonacci.AssemblyInfo.cs
+++ SequenciaFibonacci/obj/Debug/net8.0/SequenciaFibonacci.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SequenciaFibonacci")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ab2302aa9c992b2125ffd2af6f7957e12bf523")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fa61b0c9e24e1d03739cd71ef0830815ada8aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SequenciaFibonacci")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SequenciaFibonacci")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
